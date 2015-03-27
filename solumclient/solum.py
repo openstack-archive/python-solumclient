f47cba4e0620a07c0fa13f4042c49c89ed916ea7
Ed Cranford <ed.cranford@rackspace.com>
2015-03-27 11:49:07 -0500

    Make mention of resource-specific help messages
    Specifically, mention in the general "solum help" message
    that "solum languagepack help" and "solum app help" are
    available.
    
    Change-Id: I97815e415df5144c1ff71f31ac753d2d63a830d7

--- solumclient/solum.py
+++ solumclient/solum.py
@@ -854,6 +854,9 @@ Available commands:
     solum help
         Show this help message.
 
+    solum languagepack help
+        Show a help message specific to languagepack commands.
+
     solum languagepack create <NAME> <GIT_REPO_URL>
         Create a new language pack from a git repo.
 
@@ -869,6 +872,9 @@ Available commands:
     solum languagepack logs <UUID>
         Show logs for a language pack.
 
+
+    solum app help
+        Show a help message specific to app commands.
 
     solum app list
         Print an index of all deployed applications.
